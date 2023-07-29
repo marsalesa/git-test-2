c869722f351c2581ca04b310269c160344145902
marsalesa <marsalesaa@gmail.com>
2023-07-29 14:40:29 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos hic, amet sed reprehenderit neque quibusdam harum similique itaque molestiae repellat deleniti nihil odit. Accusantium sunt at commodi nulla, incidunt error?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
